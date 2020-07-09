68b03ab696db5fb6e1f8288ba226d6e08d144af3
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-07-08 18:34:43 -0600

    Bulls

--- ex40.py
+++ ex40.py
@@ -1,1 +1,23 @@
 #Python the hard way: excercise 40
+
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+    
+    def sing_me_song(self):
+        for line in self.lyrics:
+            print(line)
+    
+happy_bday = Song(["""
+    Happy birthday to you...
+    Happy birthday to you..
+    I don't want to get sued...
+"""])
+
+bulls = Song(["""
+    They rally around the family!
+    With the pockets full of shells
+"""])
+
+happy_bday.sing_me_song()
+bulls.sing_me_song()
